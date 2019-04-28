20d42cae4130a8104e520991844d17f7d3695bbb
Nilpo <devnilpo@gmail.com>
2019-04-28 00:21:24 -0400

    bumping version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='dbtsdk',
-    version='0.1',
+    version='0.2',
     author="Rob Dunham",
     author_email="devnilpo@gmail.com",
     description="DBT API SDK for Python",
