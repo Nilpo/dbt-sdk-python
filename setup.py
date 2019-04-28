730703cb09a6848369ccbededc6dc5d7131984b2
Nilpo <devnilpo@gmail.com>
2019-04-27 23:23:26 -0400

    updating setup script

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
     name='dbtsdk',
     version='0.1',
-    scripts=['dbtsdk']
+    scripts=['dbtsdk'],
+    author="Rob Dunham",
+    author_email="devnilpo@gmail.com",
+    description="DBT API SDK for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nilpo/dbt-sdk-python",
+    packages=setuptools.find.packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
 )
